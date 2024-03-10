933de0203f8d0445aa3ef25ddc30317671a9b9b0
IBebells <isabelpriscilarebello@gmail.com>
2024-03-09 22:10:03 -0400

    Atualizar parte_1 e parte_2

--- parte_1.py
+++ parte_1.py
@@ -1,21 +1,14 @@
-# Recebe duas matrizes de tamanho nx1
-# passo 1 - ordenar a matriz
-# passo 2 - verificar se é impar ou par
-
-# posicao = len(m)/2
-# posicao, posicao - 1
-
 def fusao_ordenacao_matrizes(matriz_1: list, matriz_2: list) -> list:
-
+    """Une e ordena as matrizes"""
     matriz_resultante = matriz_1 + matriz_2
     matriz_resultante.sort()
     return matriz_resultante
 
 def encontra_mediana(matriz_1: list, matriz_2: list) -> float:
-
+    """Verifica se o tamanho da matriz é impar ou par e encontra a mediana"""
     matriz_resultante = fusao_ordenacao_matrizes(matriz_1 , matriz_2)
 
-    if len(matriz_resultante) % 2 == 0: #verifica se a matriz é par
+    if len(matriz_resultante) % 2 == 0:
         posicao_ultimo_elemento_direta = int(len(matriz_resultante)/2)
         posicao_ultimo_elemento_esquerda = posicao_ultimo_elemento_direta - 1
 
@@ -28,5 +21,18 @@ def encontra_mediana(matriz_1: list, matriz_2: list) -> float:
     return mediana
 
 if __name__ == "__main__":
+    """Exemplo encontrando a mediana de uma matriz com número impar de elementos"""
+    matriz_1 = [4, 7, 2, 9, 1, 2]
+    matriz_2 = [0.9, 2.3, 5.6]
 
-    print(encontra_mediana([4, 7, 2, 9, 1, 2], [0.9, 2.3, 5.6]))
+    matriz = encontra_mediana(matriz_1, matriz_2)
+
+    print(f"A mediana é igual a: {matriz}")
+
+    """Exemplo encontrando a mediana de uma matriz com número par de elementos"""
+    matriz_1 = [4, 7, 2, 9, 1, 2]
+    matriz_2 = [0.9, 2.3, 5.6, 7.8]
+    
+    matriz = encontra_mediana(matriz_1, matriz_2)
+
+    print(f"A mediana é igual a: {matriz}")
--- parte_2.py
+++ parte_2.py
@@ -1,67 +1,39 @@
-# matriz nxn
-# passo 1 - receber n
-# passo 2 - montar uma matriz n x n
-# posicao_rainha_atual = [x][y]
 
-import numpy as np
-
-def movimento_rainha(n: int, x: int, y: int, tabuleiro: list) -> list:
-
-    soma = x + y
-
-    for i in range(n):
-        for j in range (n):
-
-            if  i + j == soma:
-                tabuleiro[i][j] = 1
-
-    for i in range(n):
-        
-        tabuleiro[i][y] = 1
-        tabuleiro[x][i] = 1
-        
-        try:
-            
-            tabuleiro[x + (i + 1)][y + (i + 1)] = 1 
-
-            if x - (i + 1) >= 0 and y - (i + 1) >= 0:
-                tabuleiro[x - (i + 1)][y - (i + 1)] = 1 
-
-        except IndexError:
-            pass
-
-    return tabuleiro
-            
-def verifica_rainha(n):
-
-    solucoes = 0
-
-    for a in range(n):
-        for b in range(n):
-
-            rainhas = 1
-            tabuleiro = np.zeros((n , n), dtype=np.int64)
-            tabuleiro = movimento_rainha(n, a, b, tabuleiro)
-            tabuleiro[a][b] = 5
-
-            for x in range(n):
-                for y in range(n):
-
-                    if tabuleiro[x][y] == 0:
-                        tabuleiro = movimento_rainha(n, x, y, tabuleiro)
-                        tabuleiro[x][y] = 5
-                        rainhas += 1
-                        verifica_rainha(n)
-                        tabuleiro[x][y] = 0
-                        
-
-                    if rainhas == n:
-                        solucoes += 1
-                        break
+def verifica_rainha(n: int, tabuleiro: list, linha_rainha: int, coluna_rainha: int) -> bool:
+    """Realiza os ataques da rainha para verificar se há uma rainha nessas posições"""
+    for linha_atual in range(linha_rainha):
+        if tabuleiro[linha_atual][coluna_rainha] == 1:
+            return False
     
-    print(solucoes)
+    for linha_atual, coluna_atual in zip(range(linha_rainha, -1, -1), range(coluna_rainha, -1, -1)):
+        if tabuleiro[linha_atual][coluna_atual] == 1:
+            return False
+    
+    for linha_atual, coluna_atual in zip(range(linha_rainha, -1, -1), range(coluna_rainha, n)):
+        if tabuleiro[linha_atual][coluna_atual] == 1:
+            return False
+    
+    return True
+            
+def encontra_solucoes(n: int, tabuleiro: list, linha_rainha: int, solucoes: list):
+    """Posiciona as rainhas no tabuleiro, e ao colocar uma rainha pula para a proxima linha.
+    Realiza esse processo de forma recurssiva para encontrar todas as soluções possíveis"""
+    if linha_rainha == n:
+        solucoes.append([linha[:] for linha in tabuleiro]) 
+        return  
 
+    for coluna_atual in range(n):
+        if verifica_rainha(n, tabuleiro, linha_rainha, coluna_atual):
+
+            tabuleiro[linha_rainha][coluna_atual] = 1
+            encontra_solucoes(n, tabuleiro, linha_rainha + 1, solucoes)
+
+            tabuleiro[linha_rainha][coluna_atual] = 0
 
 if __name__ == "__main__":
 
-    verifica_rainha(8)
+    n = 8  
+    solucoes = []
+    tabuleiro = [[0] * n for _ in range(n)]
+    encontra_solucoes(n, tabuleiro, 0, solucoes)
+    print(f"O número de soluções quando N é igual a {n} é {len(solucoes)}")
